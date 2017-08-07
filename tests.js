59ba1ad440985b8416a343e3a7c275dc049e793b
Arnelle Balane <arnellebalane@gmail.com>
2017-08-07 20:00:32 +0800

    add tests for union method

--- tests.js
+++ tests.js
@@ -1,2 +1,18 @@
 const test = require('ava');
 const UnionFind = require('./index');
+
+test('union(source, target)', (t) => {
+    const uf = new UnionFind(5);
+
+    uf.union(0, 1);
+    t.deepEqual(uf.nodes, [0, 0, 2, 3, 4]);
+
+    uf.union(2, 3);
+    t.deepEqual(uf.nodes, [0, 0, 2, 2, 4]);
+
+    uf.union(3, 4);
+    t.deepEqual(uf.nodes, [0, 0, 2, 2, 2]);
+
+    uf.union(4, 0);
+    t.deepEqual(uf.nodes, [2, 2, 2, 2, 2]);
+});
