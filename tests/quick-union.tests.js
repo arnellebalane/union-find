c4064174af4601b5c2a644bc8b2dac0b584c486d
Arnelle Balane <arnellebalane@gmail.com>
2017-08-08 00:40:25 +0800

    add tests for quick-union

---
+++ tests/quick-union.tests.js
@@ -0,0 +1,46 @@
+const test = require('ava');
+const UnionFind = require('../source/quick-union');
+
+test('constructor(size)', (t) => {
+    const uf = new UnionFind(5);
+    const expected = [0, 1, 2, 3, 4];
+    t.deepEqual(uf.nodes, expected);
+});
+
+test('union(source, target)', (t) => {
+    const uf = new UnionFind(5);
+
+    uf.union(0, 1);
+    t.deepEqual(uf.nodes, [0, 0, 2, 3, 4]);
+
+    uf.union(2, 3);
+    t.deepEqual(uf.nodes, [0, 0, 2, 2, 4]);
+
+    uf.union(3, 4);
+    t.deepEqual(uf.nodes, [0, 0, 2, 2, 3]);
+
+    uf.union(4, 0);
+    t.deepEqual(uf.nodes, [4, 0, 2, 2, 3]);
+});
+
+test('connected(source, target)', (t) => {
+    const uf = new UnionFind(5);
+
+    t.false(uf.connected(0, 1));
+    t.false(uf.connected(0, 4));
+
+    uf.union(0, 1);
+    t.true(uf.connected(0, 1));
+
+    uf.union(2, 3);
+    t.true(uf.connected(2, 3));
+
+    uf.union(3, 4);
+    t.true(uf.connected(3, 4));
+    t.true(uf.connected(2, 4));
+
+    uf.union(4, 0);
+    t.true(uf.connected(4, 0));
+    t.true(uf.connected(1, 4));
+    t.true(uf.connected(1, 3));
+});
