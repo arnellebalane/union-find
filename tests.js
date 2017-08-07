caca6203700d26934a0906af16e6765ac2061f93
Arnelle Balane <arnellebalane@gmail.com>
2017-08-07 20:06:03 +0800

    add tests for connected method

--- tests.js
+++ tests.js
@@ -22,3 +22,25 @@ test('union(source, target)', (t) => {
     uf.union(4, 0);
     t.deepEqual(uf.nodes, [2, 2, 2, 2, 2]);
 });
+
+test('connected(source, target)', (t) => {
+    const uf = new UnionFind(5);
+
+    t.false(uf.connected(0, 1));
+    t.false(uf.connected(0, 4));
+
+    uf.union(0, 1);
+    t.true(uf.connected(0, 1));
+
+    uf.union(2, 3);
+    t.true(uf.connected(2, 3));
+
+    uf.union(3, 4);
+    t.true(uf.connected(3, 4));
+    t.true(uf.connected(2, 4));
+
+    uf.union(4, 0);
+    t.true(uf.connected(4, 0));
+    t.true(uf.connected(1, 4));
+    t.true(uf.connected(1, 3));
+});
