969d0fa9b31635ab1b55389a2c03f2d2bc286152
Arnelle Balane <arnellebalane@gmail.com>
2017-08-08 10:48:40 +0800

    update quick-find tests to match coursera test cases

--- tests/quick-find.tests.js
+++ tests/quick-find.tests.js
@@ -2,45 +2,48 @@ const test = require('ava');
 const UnionFind = require('../source/quick-find');
 
 test('constructor(size)', (t) => {
-    const uf = new UnionFind(5);
-    const expected = [0, 1, 2, 3, 4];
+    const uf = new UnionFind(10);
+    const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     t.deepEqual(uf.nodes, expected);
 });
 
 test('union(source, target)', (t) => {
-    const uf = new UnionFind(5);
+    const uf = new UnionFind(10);
 
-    uf.union(0, 1);
-    t.deepEqual(uf.nodes, [0, 0, 2, 3, 4]);
+    uf.union(4, 3);
+    t.deepEqual(uf.nodes, [0, 1, 2, 3, 3, 5, 6, 7, 8, 9]);
 
-    uf.union(2, 3);
-    t.deepEqual(uf.nodes, [0, 0, 2, 2, 4]);
+    uf.union(3, 8);
+    t.deepEqual(uf.nodes, [0, 1, 2, 8, 8, 5, 6, 7, 8, 9]);
 
-    uf.union(3, 4);
-    t.deepEqual(uf.nodes, [0, 0, 2, 2, 2]);
+    uf.union(6, 5);
+    t.deepEqual(uf.nodes, [0, 1, 2, 8, 8, 5, 5, 7, 8, 9]);
 
-    uf.union(4, 0);
-    t.deepEqual(uf.nodes, [2, 2, 2, 2, 2]);
+    uf.union(9, 4);
+    t.deepEqual(uf.nodes, [0, 1, 2, 8, 8, 5, 5, 7, 8, 8]);
+
+    uf.union(2, 1);
+    t.deepEqual(uf.nodes, [0, 1, 1, 8, 8, 5, 5, 7, 8, 8]);
+
+    uf.union(5, 0);
+    t.deepEqual(uf.nodes, [0, 1, 1, 8, 8, 0, 0, 7, 8, 8]);
+
+    uf.union(7, 2);
+    t.deepEqual(uf.nodes, [0, 1, 1, 8, 8, 0, 0, 1, 8, 8]);
+
+    uf.union(6, 1);
+    t.deepEqual(uf.nodes, [1, 1, 1, 8, 8, 1, 1, 1, 8, 8]);
 });
 
 test('connected(source, target)', (t) => {
-    const uf = new UnionFind(5);
+    const uf = new UnionFind(10);
 
-    t.false(uf.connected(0, 1));
-    t.false(uf.connected(0, 4));
+    uf.union(4, 3);
+    uf.union(3, 8);
+    uf.union(6, 5);
+    uf.union(9, 4);
+    uf.union(2, 1);
 
-    uf.union(0, 1);
-    t.true(uf.connected(0, 1));
-
-    uf.union(2, 3);
-    t.true(uf.connected(2, 3));
-
-    uf.union(3, 4);
-    t.true(uf.connected(3, 4));
-    t.true(uf.connected(2, 4));
-
-    uf.union(4, 0);
-    t.true(uf.connected(4, 0));
-    t.true(uf.connected(1, 4));
-    t.true(uf.connected(1, 3));
+    t.true(uf.connected(8, 9));
+    t.false(uf.connected(5, 0));
 });
