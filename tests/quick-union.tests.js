2b58cf95b338f01b3596dc06980e8028df7113bc
Arnelle Balane <arnellebalane@gmail.com>
2017-08-08 10:31:41 +0800

    update quick-union tests to match coursera test cases

--- tests/quick-union.tests.js
+++ tests/quick-union.tests.js
@@ -2,29 +2,44 @@ const test = require('ava');
 const UnionFind = require('../source/quick-union');
 
 test('constructor(size)', (t) => {
-    const uf = new UnionFind(5);
-    const expected = [0, 1, 2, 3, 4];
+    const uf = new UnionFind(10);
+    const expected = [0, 1, 2, 3, 4, 6, 7, 8, 9];
     t.deepEqual(uf.nodes, expected);
 });
 
 test('union(source, target)', (t) => {
-    const uf = new UnionFind(5);
+    const uf = new UnionFind(10);
 
-    uf.union(0, 1);
-    t.deepEqual(uf.nodes, [0, 0, 2, 3, 4]);
+    uf.union(4, 3);
+    t.deepEqual(uf.nodes, [0, 1, 2, 3, 3, 5, 6, 7, 8, 9]);
 
-    uf.union(2, 3);
-    t.deepEqual(uf.nodes, [0, 0, 2, 2, 4]);
+    uf.union(3, 8);
+    t.deepEqual(uf.nodes, [0, 1, 2, 8, 3, 5, 6, 7, 8, 9]);
 
-    uf.union(3, 4);
-    t.deepEqual(uf.nodes, [0, 0, 2, 2, 3]);
+    uf.union(6, 5);
+    t.deepEqual(uf.nodes, [0, 1, 2, 8, 3, 5, 5, 7, 8, 9]);
 
-    uf.union(4, 0);
-    t.deepEqual(uf.nodes, [4, 0, 2, 2, 3]);
+    uf.union(9, 4);
+    t.deepEqual(uf.nodes, [0, 1, 2, 8, 3, 5, 5, 7, 8, 8]);
+
+    uf.union(2, 1);
+    t.deepEqual(uf.nodes, [0, 1, 1, 8, 3, 5, 5, 7, 8, 8]);
+
+    uf.union(5, 0);
+    t.deepEqual(uf.nodes, [0, 1, 1, 8, 3, 0, 5, 7, 8, 8]);
+
+    uf.union(7, 2);
+    t.deepEqual(uf.nodes, [0, 1, 1, 8, 3, 0, 5, 1, 8, 8]);
+
+    uf.union(6, 1);
+    t.deepEqual(uf.nodes, [1, 1, 1, 8, 3, 0, 5, 1, 8, 8]);
+
+    uf.union(7, 3);
+    t.deepEqual(uf.nodes, [1, 8, 1, 8, 3, 0, 5, 1, 8, 8]);
 });
 
 test('connected(source, target)', (t) => {
-    const uf = new UnionFind(5);
+    const uf = new UnionFind(10);
 
     t.false(uf.connected(0, 1));
     t.false(uf.connected(0, 4));
