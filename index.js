ecdc4d0e878cc48d2f2707687a5ff09cc49eefc6
Arnelle Balane <arnellebalane@gmail.com>
2017-08-09 18:20:26 +0800

    implement weighted quick union with path compression

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.QuickFindUF = require('./source/quick-find');
 exports.QuickUnionUF = require('./source/quick-union');
 exports.WeightedQuickUnionUF = require('./source/weighted-quick-union');
+exports.PathCompressionUF = require('./source/path-compression');
---
+++ source/path-compression.js
@@ -0,0 +1,35 @@
+class UnionFind {
+    constructor(size) {
+        this.nodes = [];
+        this.sizes = new Array(size).fill(1);
+        for (let i = 0; i < size; i++) {
+            this.nodes.push(i);
+        }
+    }
+
+    union(source, target) {
+        const sourceRoot = this.root(source);
+        const targetRoot = this.root(target);
+        if (this.sizes[sourceRoot] < this.sizes[targetRoot]) {
+            this.nodes[sourceRoot] = targetRoot;
+            this.sizes[targetRoot] += this.sizes[sourceRoot];
+        } else {
+            this.nodes[targetRoot] = sourceRoot;
+            this.sizes[sourceRoot] += this.sizes[targetRoot];
+        }
+    }
+
+    connected(source, target) {
+        return this.root(source) === this.root(target);
+    }
+
+    root(node) {
+        const parent = this.nodes[node];
+        while (parent !== node) {
+            [node, this.nodes[node]] = [parent, this.nodes[parent]];
+        }
+        return node;
+    }
+}
+
+module.exports = UnionFind;
